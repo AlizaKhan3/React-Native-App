558b1a89818ea419e6e06e6165bae0223cad5869
Aliza Khan <alizaadi2@gmail.com>
2025-09-06 22:24:19 +0500

    Set up firebase and react native toast

--- App.jsx
+++ App.jsx
@@ -1,13 +1,17 @@
 // import { NewAppScreen } from '@react-native/new-app-screen';
 import { View, Text } from 'react-native';
 import AppNavigation from "./AppNavigation"
+import Toast from 'react-native-toast-message';
 
 function App() {
   return (
     //  <View>
     //   {/* <Text style={{paddingHorizontal: 9, paddingVertical: 30}}>ALIZA KHAN</Text> */}
     //  </View>
-    <AppNavigation />
+    <>
+      <AppNavigation />
+      <Toast />
+    </>
   );
 }
 export default App;
